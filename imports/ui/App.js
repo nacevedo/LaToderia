c82cc01a15755cb914451968520d2eb35a7f5148
nacevedo <n.acevedos@uniandes.edu.co>
2018-04-30 21:26:11 -0500

    intento de D3

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -13,6 +13,8 @@ import Post from "./Post";
 import PostAlone from "./PostAlone";
 import ChatList from "./ChatList";
 import ChatAlone from "./ChatAlone";
+import Ranking from "./Ranking";
+
 
 
 export class App extends Component {
@@ -84,76 +86,15 @@ export class App extends Component {
   }
 
   render() {
-    return (
-      <HashRouter>
-        <div className="App">
-          <div className="navbar-header">
-            <div className="row">
-            
-                <button type="button" 
-                        className="navbar-toggle collapsed"
-                        data-toggle="collapse" 
-                        data-target="#collapsable-nav" 
-                        aria-expanded="false">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="fa">&#xf0c9;</span>
-                </button>
-              
-            </div>
-          </div>
-          <div id="collapsable-nav" className="collapse navbar-collapse">
-            <ul id="nav-list" className="header ">
-              <li>
-                <NavLink exact to="/">Home</NavLink>
-              </li>
-              <li>
-                <NavLink to="/city">{this.state.city}</NavLink>
-              </li>
-              <li>
-                <NavLink to="/post">{this.state.postName}</NavLink>
-              </li>
-              <li>
-                <NavLink to="/chat">Current Chats</NavLink>
-              </li>
-              <li>
-                <NavLink to="/chatchat">{this.state.user1} &nbsp; {this.state.user2}</NavLink>
-              </li>
-              <li id="sign-in-place" >
-                <AccountsUIWrapper />
-              </li>
-            </ul>
-          </div>
-          <h1><span className="fa">&#xf1d8;</span>&nbsp; Mandao Dao</h1>
-          <hr/>
-          <div className="content">
-            <Route exact path="/" render={()=>
-            <Home updateCity={this.onChangeCity.bind(this)} />
-            } />
-            <Route path="/city/" render={()=>
-            <Stuff city={this.state.city} 
-              updatePostID={this.onChangePostID.bind(this)} 
-              updatePostName={this.onChangePost.bind(this)} 
-              search = {this.state.search}
-              filter = {this.filter.bind(this)} />
-            } />
-            <Route path="/post/" render={()=>
-            <PostAlone postID={this.state.postID} />
-            } />
-            <Route path="/chat/" render={()=>
-            <ChatList updateChatID={this.onChangeChatID.bind(this)}
-                      updateUser1={this.onChangeUser1.bind(this)}
-                      updateUser2={this.onChangeUser2.bind(this)}/>
-            } />
-            <Route path="/chatchat/" render={()=>
-            <ChatAlone chatID={this.state.chatID}
-                        u1={this.state.user1}
-                        u2={this.state.user2}/>
-            } />
-          </div>
-        </div>
-      </HashRouter>
-    );
-  }
+    return(
+      <div> 
+        <div> EL app </div> 
+        <Ranking/>
+      </div> 
+      ); 
+  
+  
+ }
 }
 
 App.propTypes = {
---
+++ imports/ui/Ranking.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import * as d3 from "d3"; 
+
+
+class Ranking extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  componentDidMount(){
+  	const svg = d3.select(this.svg);
+    this.width = +svg.attr("width");
+    this.height = +svg.attr("height");
+
+	this.format = d3.format(",d");
+
+	this.color = d3.scaleOrdinal(d3.schemeCategory20c);
+
+	this.pack = d3.pack()
+	    .size([this.width, this.height])
+	    .padding(1.5);
+
+
+	this.update(this.props); 
+	}
+
+	
+
+	update (props){
+	console.log("Update", props); 
+	console.log("State", this.svg); 
+		//if (!props.posts || props.post.length === 0) return ; 
+
+		const data = [
+			{
+			name : "pepito",
+			cal : 2 
+			}, 
+			{
+			name : "juanito",
+			cal : 3
+			}, 
+			{
+			name : "doricita",
+			cal : 3
+			}
+	];  
+
+		  var root = d3.hierarchy({children: classes})
+		      .sum(function(d) { return d.value; }) 
+		      .each(function(d) {
+		        if (id = d.data.id) {
+		          var id, i = id.lastIndexOf(".");
+		          d.id = id;
+		          d.package = id.slice(0, i);
+		          d.class = id.slice(i + 1);
+		        }
+		      });
+
+		  var node = this.svg.selectAll(".node")
+		    .data(pack(root).leaves())
+		    .enter().append("g")
+		      .attr("class", "node")
+		      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+		  node.append("circle")
+		      .attr("id", function(d) { return d.id; })
+		      .attr("r", function(d) { return d.r; })
+		      .style("fill", function(d) { return color(d.package); });
+
+		  node.append("clipPath")
+		      .attr("id", function(d) { return "clip-" + d.id; })
+		    .append("use")
+		      .attr("xlink:href", function(d) { return "#" + d.id; });
+
+		  node.append("text")
+		      .attr("clip-path", function(d) { return "url(#clip-" + d.id + ")"; })
+		    .selectAll("tspan")
+		    .data(function(d) { return d.class.split(/(?=[A-Z][^A-Z])/g); })
+		    .enter().append("tspan")
+		      .attr("x", 0)
+		      .attr("y", function(d, i, nodes) { return 13 + (i - nodes.length / 2 - 0.5) * 10; })
+		      .text(function(d) { return d; });
+
+		  node.append("title")
+		      .text(function(d) { return d.id + "\n" + format(d.value); });
+
+
+	
+  }
+
+  render() {
+  	return (
+    <div> 
+    <svg width="960" 
+    	height="960" 
+    	ref = {(svg) => this.svg = svg}>
+    </svg>
+
+    </div> 
+   ); 
+  }
+}
+
+Ranking.propTypes = {
+  
+};
+
+export default(Ranking);
