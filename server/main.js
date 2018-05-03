0cda4030d7a889dc80326a85619649728543c663
Bibiana <b.gamba10@uniandes.edu.co>
2018-05-02 22:13:54 -0500

    lang

--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { Inject } from "meteor/meteorhacks:inject-initial";
 
 import "../imports/api/posts";
 import "../imports/api/chatMessages";
@@ -8,6 +9,8 @@ import "../imports/api/comments";
 
 Meteor.startup(() => {
   // code to run on server at startup
-
+   Inject.rawModHtml("addLanguage", function(html) {
+     return html.replace(/<html>/, '<!-- HTML 5 -->\n<html lang="es">');
+   });
 
 });
