928c45db6ca99366a3733a1213d4f9ce7e0e698b
nacevedo <n.acevedos@uniandes.edu.co>
2018-05-18 23:37:11 -0500

    no duplicates

--- imports/api/tweets.js
+++ imports/api/tweets.js
@@ -44,7 +44,8 @@ Meteor.methods({
                     author: data["user"]["name"],
                     avatar: data["user"]["profile_image_url"],
                     body: data["text"],
-                    screenname: data["user"]["screen_name"]
+                    screenname: data["user"]["screen_name"],
+                    _id: data["id"]
                 };
 
                 new SimpleSchema({
